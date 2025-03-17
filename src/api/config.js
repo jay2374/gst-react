f2cc5129e8995e9a0388dedf7c5dc79bf63d20c8
ASUS <jay2374@gmail.com>
2025-03-17 14:45:36 +0530

    Updated security policy

--- src/api/config.js
+++ src/api/config.js
@@ -3,12 +3,12 @@ const getApiBaseUrl = () => {
     return process.env.REACT_APP_API_URL;
   }
   // Use HTTPS for production
-  return 'https://jht2s256ce.execute-api.ap-south-1.amazonaws.com';
+  return 'https://tihnthwonf.execute-api.ap-south-1.amazonaws.com';
 };
 
 export const endpoints = {
-  signup: `https://jht2s256ce.execute-api.ap-south-1.amazonaws.com/prod/api/signup`,
-  signin: `https://jht2s256ce.execute-api.ap-south-1.amazonaws.com/prod/api/signin`,
+  signup: `https://tihnthwonf.execute-api.ap-south-1.amazonaws.com/prod/api/signup`,
+  signin: `https://tihnthwonf.execute-api.ap-south-1.amazonaws.com/prod/api/signin`,
 };
 
 export default endpoints; 
